e544394bb9c426eda61721b3219e9f7bf9693d7e
Varannchik <varannchik@gmail.com>
2019-06-18 23:07:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+let elem=document.querySelector('.container');
+
+let btn=document.querySelector('button');
+
+
+
+
+
+let i=1;
+btn.addEventListener('click',function(){
+    let p=document.createElement('p');
+    let input=document.querySelector('input').value; 
+    let bMinus=document.createElement('button');
+    let span=document.createElement('span'); 
+    let arr2=document.querySelectorAll('span'); 
+    elem.appendChild(p);
+    p.innerHTML=input;
+    p.appendChild(bMinus);
+    p.appendChild(span);
+    bMinus.innerHTML='-';
+
+    for(let i=0; i<arr2.length; i++){
+        arr2[i].innerHTML=''+(i+1);
+    }
+    
+
+    bMinus.addEventListener('click',function(){    
+        elem.removeChild(p);
+        let arr=document.querySelectorAll('span');        
+        for(let j=0; j<arr.length; j++){
+            arr[j].innerHTML=''+(j+1);            
+        }
+    });
+       
+});
+
+
+
+
+
+
+
+
+
+
+
