d5dfa2505751adc0bd9ee400eca37176da777d53
Varannchik <varannchik@gmail.com>
2019-06-18 23:43:29 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 let elem=document.querySelector('.container');
 let btn=document.querySelector('button');
-let i=1;
 btn.addEventListener('click',function(){
     let p=document.createElement('p');
     let input=document.querySelector('input').value; 
     let bMinus=document.createElement('button');
-    let span=document.createElement('span'); 
-    let arr2=document.querySelectorAll('span'); 
+    let span=document.createElement('span');
+    
     elem.appendChild(p);
     p.innerHTML=input;
     p.appendChild(bMinus);
     p.appendChild(span);
     bMinus.innerHTML='-';
-    
-    for(let i=0; i<=arr2.length; i++){
-        arr2[i].innerHTML=''+(i+1);
-    }   
+    let arr2=document.querySelectorAll('span'); 
+    for(let i=0; i<arr2.length; i++){
+        arr2[i].innerHTML=''+(i+1);        
+        
+    } 
 
     bMinus.addEventListener('click',function(){    
         elem.removeChild(p);
@@ -33,7 +33,7 @@ btn.addEventListener('click',function(){
 
 
 
-
+//div2.removeChild(h3);
 
 
 
