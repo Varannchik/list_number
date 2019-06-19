faf1ec256279a5e0667069145ded276013c4576d
Varannchik <varannchik@gmail.com>
2019-06-19 22:20:13 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 let elem=document.querySelector('.container');
+let h2=document.createElement('h2');
 let btn=document.querySelector('button');
+let div2=document.querySelector('.dqs');
+
 btn.addEventListener('click',function(){
     let p=document.createElement('p');
     let input=document.querySelector('input').value; 
     let bMinus=document.createElement('button');
     let span=document.createElement('span');
-    
-    elem.appendChild(p);
+    let bArrow=document.createElement('button');
+    div2.appendChild(p);
     p.innerHTML=input;
     p.appendChild(bMinus);
+    p.appendChild(bArrow);
     p.appendChild(span);
     bMinus.innerHTML='-';
+    bArrow.innerHTML='arrow';
+    
     let arr2=document.querySelectorAll('span'); 
     for(let i=0; i<arr2.length; i++){
         arr2[i].innerHTML=''+(i+1);        
@@ -18,13 +24,16 @@ btn.addEventListener('click',function(){
     } 
 
     bMinus.addEventListener('click',function(){    
-        elem.removeChild(p);
+        div2.removeChild(p);
         let arr=document.querySelectorAll('span');        
         for(let j=0; j<=arr.length; j++){
             arr[j].innerHTML=''+(j+1);            
         }
     });
-       
+
+        bArrow.addEventListener('click',function(){        
+        div2.insertBefore(p, p.previousElementSibling);
+    });        
 });
 
 
